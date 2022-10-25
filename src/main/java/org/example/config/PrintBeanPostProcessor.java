a7aeb0ace228da839550281a6a61d1a459d2161c
jalivv <shizuwei99@gmail.com>
2022-10-25 10:11:25 +0800

    1.0

--- src/main/java/org/example/config/PrintBeanPostProcessor.java
+++ src/main/java/org/example/config/PrintBeanPostProcessor.java
@@ -15,11 +15,6 @@ import org.springframework.stereotype.Component;
 public class PrintBeanPostProcessor implements BeanPostProcessor {
 
     @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
-    }
-
-    @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if(bean.getClass().getSuperclass().isAnnotationPresent(EnablePrint.class)){
             System.out.println(bean.getClass().getSuperclass().getAnnotation(EnablePrint.class).value());
